4cf49ab7078edce9f4234790fd74fb7718b0d0ed
s0161026 <s0161026@gmail.com>
2020-09-08 00:30:54 -0500

    add BE files

---
+++ whatsapp-backend/server.js
@@ -0,0 +1,49 @@
+// importing
+import express from "express"
+import mongoose from "mongoose"
+import Messages from "./dbMessages.js"
+
+// app config
+const app = express()
+const port = process.env.PORT || 9000
+
+// middlerware
+app.use(express.json())
+
+// DB config
+const connection_url = 'mongodb+srv://admin:3u3Dk9dO8RyUKW7H@cluster0.k8yiq.mongodb.net/whatsappdb?retryWrites=true&w=majority' 
+mongoose.connect(connection_url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+// ????
+
+// api routes
+app.get('/', (req, res) => res.status(200).send('hello world'))
+
+app.get('/messages/sync', (req, res) => {
+    Messages.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.post('/messages/new', (req, res) => {
+    const dbMessage = req.body
+
+    Messages.create(dbMessage, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
+
+// listen
+app.listen(port, () => console.log(`Listening on localhost:${port}`))
